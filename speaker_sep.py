4232d1bb26eaf28fd4ea5bf1cd16b5bc04d67974
sleekEagle <lahirunuwanisme@gmail.com>
2025-12-11 18:43:21 +0530

    speaker seperation as a class

--- speaker_sep.py
+++ speaker_sep.py
@@ -1,8 +1,18 @@
 from clearvoice import ClearVoice
 
-myClearVoice = ClearVoice(
-    task='target_speaker_extraction',
-    model_names=['AV_MossFormer2_TSE_16K']  # This model supports audio-only
+class AudioProcessor:
+    def __init__(self):
+        self.processor = ClearVoice(task='speech_separation',  model_names=['MossFormer2_SS_16K'])
+    
+    def process_file(self, input_path):
+        output = self.processor(input_path=input_path, online_write=False)
+        return output
+    
+'''
+how to use:
+ap = AudioProcessor()
+output = ap.process_file(
+    input_path=r'C:\Users\lahir\code\CREMA-D\AudioWAV\1001_DFA_SAD_XX.wav',
 )
+'''
 
-pass
