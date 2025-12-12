ee509d214468e3466eb96d170cc68797a847d94e
sleekEagle <lahirunuwanisme@gmail.com>
2025-12-12 18:53:55 +0530

    updates

--- create_noisy_dataset.py
+++ create_noisy_dataset.py
@@ -5,14 +5,16 @@ from dataloaders.cremad_hf import get_split_paths
 
 data_path = r'C:\Users\lahir\code\CREMA-D\AudioWAV'
 noise_path = r'C:\Users\lahir\data\noise\speech_noise.mp3'
-output_path = r'C:\Users\lahir\code\CREMA-D\speech_noise_db15_whole'
+output_path = r'C:\Users\lahir\code\CREMA-D\speech_noise_db15_eval'
 os.makedirs(output_path, exist_ok=True)
 
 
 train_paths, eval_paths = get_split_paths(data_path)
-for i,file in enumerate(train_paths + eval_paths):
+for i,file in enumerate(eval_paths):
+    out_file = os.path.join(output_path, os.path.basename(file))
+    if os.path.exists(out_file):
+        continue
     print(f'Processing file {i+1}/{len(eval_paths)}')
     noisy = mix_noise.mix_noise(file, noise_path, snr_db=15)
-    out_file = os.path.join(output_path, os.path.basename(file))
     sf.write(out_file, noisy, 16000)
 
