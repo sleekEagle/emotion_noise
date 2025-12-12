16ab1d4b987d5a12f91eb8a4d55f3b388c70303c
sleekEagle <lahirunuwanisme@gmail.com>
2025-12-12 12:36:02 +0530

    updates

--- dataloaders/cremad_hf.py
+++ dataloaders/cremad_hf.py
@@ -6,6 +6,8 @@ import numpy as np
 
 data_dir = r'C:\Users\lahir\code\CREMA-D\AudioWAV'
 feature_extractor = AutoFeatureExtractor.from_pretrained("facebook/wav2vec2-base")
+MAX_LEN = 16000
+SR = 16000
 
 EMOTION_MAP = {
     "ANG": 0,  # angry
@@ -19,22 +21,13 @@ EMOTION_MAP = {
 def preprocess_function(examples):
     audio_arrays = [x["array"] for x in examples["audio"]]
     inputs = feature_extractor(
-        audio_arrays, sampling_rate=feature_extractor.sampling_rate, max_length=16000, truncation=True
+        audio_arrays, sampling_rate=feature_extractor.sampling_rate, max_length=MAX_LEN, truncation=True
     )
     return inputs
 
 def get_hf_dataset():
-    paths = []
-    labels = []
-    sub = []
-
-    
-    for file in os.listdir(data_dir):
-        label = file.split('_')[2]
-        sub.append(file.split('_')[0])
-        paths.append(os.path.abspath(os.path.join(data_dir, file)))
-        labels.append(label)
-
+    paths, labels, sub = get_paths()
+    subject_names = ClassLabel(names=sorted(set(sub)))
     ds = Dataset.from_dict({"audio": paths, "label": labels, "sub": sub})
     hf_dataset = ds.cast_column("audio", Audio())
     hf_dataset = hf_dataset.cast_column("label", ClassLabel(names=sorted(set(labels))))
@@ -51,15 +44,53 @@ def get_hf_dataset():
     # hf_dataset = hf_dataset.cast_column("label", class_label)
 
     #seperate splits
-    ids = sorted(list(set(hf_dataset['sub'])))
-    ids.sort()
-    train_subjects = ids[: int(0.8 * len(ids))]
-    eval_subjects  = ids[int(0.8 * len(ids)) :]
+    train_subjects, eval_subjects = create_splits()
 
     train_ds = hf_dataset.filter(lambda row: row["sub"] in train_subjects)
     eval_ds = hf_dataset.filter(lambda row: row["sub"] in eval_subjects)
 
-    return train_ds, eval_ds
+    return train_ds, eval_ds, subject_names.names
 
 
 # get_hf_dataset()
+
+def get_paths():
+    paths = []
+    labels = []
+    sub = []
+
+    for file in os.listdir(data_dir):
+        label = file.split('_')[2]
+        sub.append(file.split('_')[0])
+        paths.append(os.path.abspath(os.path.join(data_dir, file)))
+        labels.append(label)
+    return paths, labels, sub
+
+def create_splits():
+    paths, labels, sub = get_paths()
+    subjects = list(set(sub))
+    subjects.sort()
+    train_subjects = subjects[: int(0.8 * len(subjects))]
+    eval_subjects  = subjects[int(0.8 * len(subjects)) :]
+    return train_subjects, eval_subjects
+
+def get_split_paths():
+    train_subjects, eval_subjects = create_splits()
+    train_paths = []
+    eval_paths = []
+
+    for file in os.listdir(data_dir):
+        subject = file.split('_')[0]
+        full_path = os.path.abspath(os.path.join(data_dir, file))
+        if subject in train_subjects:
+            train_paths.append(full_path)
+        else:
+            eval_paths.append(full_path)
+    return train_paths, eval_paths
+
+
+
+
+
+if __name__ == "__main__":
+    get_split_paths()
